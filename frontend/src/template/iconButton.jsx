8ec4c8366dd66204dd131822a95a5141cbeacf74
Eduardo <edwardviana@gmail.com>
2018-10-14 21:02:53 -0300

    modificações

--- frontend/src/template/iconButton.jsx
+++ frontend/src/template/iconButton.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import If from './if'
-export default props =>{
-    return(<If test={!props.hide}> 
-        <button className={'btn btn-'+ props.style}
-          onClick={props.onClick}>
-            <i className={`fa fa-` + props.icon}></i>
-        </button>
-    </If>) 
-}
+import React from "react";
+import If from "./if";
+export default props => {
+  return (
+    <If test={!props.hide}>
+      <button className={"btn btn-" + props.style} onClick={props.onClick}>
+        <i className={`fa fa-` + props.icon} />
+      </button>
+    </If>
+  );
+};
